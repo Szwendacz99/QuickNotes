cf279ca68abd23b2220d9e92fb4e3b53a56acfbd
Maciej Lebiest <maciejlebiest@protonmail.com>
2021-10-13 18:56:07 +0200

    initial commit v3

--- quicknotes/index.php
+++ quicknotes/index.php
@@ -1,3 +1,4 @@
 <?php
 $name = "Maciej";
-echo "<marquee style='font-size:500px; color:red;'>$name</marquee>";
+echo "<marquee scrollamount='70' style='font-size:100px; color:red;'>$name</marquee>";
+phpinfo();
