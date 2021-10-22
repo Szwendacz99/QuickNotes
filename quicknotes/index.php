68a12502dd8d11e6aad447b5583e95fd6c8ff1da
Maciej Lebiest <maciejlebiest@protonmail.com>
2021-10-22 14:50:29 +0200

    add background picture and share icon

--- quicknotes/index.php
+++ quicknotes/index.php
@@ -1,4 +1,4 @@
 <?php
 $name = "Maciej";
-echo "<marquee scrollamount='70' style='font-size:100px; color:red;'>$name</marquee>";
+echo "<marquee scrollamount='2000' style='font-size:100px; color:red;'>$name</marquee>";
 phpinfo();
