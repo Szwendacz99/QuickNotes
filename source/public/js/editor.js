4be00090da5af5cf058ff3b4dc06861936580b7f
Maciej Lebiest <maciejlebiest@protonmail.com>
2021-11-08 14:22:45 +0100

    embed left panel in editor view

---
+++ source/public/js/editor.js
@@ -0,0 +1,7 @@
+function switchLeftPanel() {
+    if (document.getElementById("left-panel").style.display == "none") {
+        document.getElementById("left-panel").style.display = "flex";
+    } else {
+        document.getElementById("left-panel").style.display = "none";
+    }
+}
