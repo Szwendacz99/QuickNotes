ea30285b5ca4288cf6e5eb9a82ce639e9c697ad5
Maciej Lebiest <maciejlebiest@protonmail.com>
2021-11-15 16:11:32 +0100

    add basic overlay css

--- source/public/js/editor.js
+++ source/public/js/editor.js
@@ -5,3 +5,13 @@ function switchDisplay(id, displayType) {
         document.getElementById(id).style.display = "none";
     }
 }
+
+function switchOverlay(overlayBgId, overlayId, displayType) {
+    if (document.getElementById(overlayId).style.display === "none") {
+        document.getElementById(overlayId).style.display = displayType;
+        document.getElementById(overlayBgId).style.display = displayType;
+    } else {
+        document.getElementById(overlayId).style.display = "none";
+        document.getElementById(overlayBgId).style.display = "none";
+    }
+}
